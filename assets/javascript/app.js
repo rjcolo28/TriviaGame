6fb317e64a1c50a54753b5cd80210c0c9671e0d0
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-12-05 13:08:17 -0500

    Variables and start function set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,21 @@
 // Global Variables
-var questionHeaders = ["Question 1,", "Question 2,", "Question 3,", "Question 4,", "Question 5,", "Question 6,", "Question 7,", "Question 8,", "Question 9,", "Question 10,", "Question 11,", "Question 12,", "Question 13,", "Question 14,", "Question 15,"];
+var questionHeaders = [
+    "Question 1", 
+    "Question 2", 
+    "Question 3", 
+    "Question 4", 
+    "Question 5", 
+    "Question 6", 
+    "Question 7", 
+    "Question 8", 
+    "Question 9", 
+    "Question 10", 
+    "Question 11", 
+    "Question 12", 
+    "Question 13", 
+    // "Question 14", 
+    // "Question 15"
+];
 
 var questions = {
     q1: "What are superpowers called in My Hero Academia?",
@@ -15,39 +31,48 @@ var questions = {
     q11: "Who becomes the new Number 1 hero after All Might’s retirement?",
     q12: "What is All Might’s real name?",
     q13: "True or False? One for All has been passed down through 8 generations.",
-    q14: "",
-    q15: "",
+    // q14: "",
+    // q15: "",
 };
 
 var answers = {
-    a1 =["a. superpowers", "b. powers", "c. quirks", "d. magic"],
-    a2 =["a. All Might", "b. Eraserhead", "c. Best Jeanist", "d. Mt. Lady"],
-    a3 =["a. All for One ", "b. One for All", "c. Super Smash", "d. Justice Power"],
-    a4 =["a. True", "b. False"],
-    a5 =["a. A pseudo-scientfic mix of chemicals and a lightning strike.", "b. He has engines on his calves.", "c. He's just really fast"],
-    a6 =["a. Izuku Midoriya", "b. Shoto Todoroki", "c. Momo Yaoyorozu", "d. Tenya Iida"],
-    a7 =["a. Erasure", "b. Delete", "c. Nullify", "d. Sleep"],
-    a8 =["a. Dabi", "b. Kurogiri", "c. All for One", "d. Tomura Shigaraki"],
-    a9 =["a. True", "b. False"],
-    a10 =["a. Kamui Woods", "b. Mt. Lady", "c. Death Arms", "d. Backdraft"],
-    a11 =["a. Best Jeanist", "b. Gang Orca", "c. Kamui Woods", "d. Endeavor"],
-    a12 =["a. Takuma Ogata", "b. Natsu Maebara", "c. Yagi Toshinori", "d. Akeno Harada"],
-    a13 =["a. True", "b. False"],
-    a3 =["a. ", "b. ", "c. ", "d. "],
-    a3 =["a. ", "b. ", "c. ", "d. "]
+    a1: ["a. superpowers", "b. powers", "c. quirks", "d. magic"],
+    a2: ["a. All Might", "b. Eraserhead", "c. Best Jeanist", "d. Mt. Lady"],
+    a3: ["a. All for One ", "b. One for All", "c. Super Smash", "d. Justice Power"],
+    a4: ["a. True", "b. False"],
+    a5: ["a. A pseudo-scientfic mix of chemicals and a lightning strike.", "b. He has engines on his calves.", "c. He's just really fast"],
+    a6: ["a. Izuku Midoriya", "b. Shoto Todoroki", "c. Momo Yaoyorozu", "d. Tenya Iida"],
+    a7: ["a. Erasure", "b. Delete", "c. Nullify", "d. Sleep"],
+    a8: ["a. Dabi", "b. Kurogiri", "c. All for One", "d. Tomura Shigaraki"],
+    a9: ["a. True", "b. False"],
+    a10: ["a. Kamui Woods", "b. Mt. Lady", "c. Death Arms", "d. Backdraft"],
+    a11: ["a. Best Jeanist", "b. Gang Orca", "c. Kamui Woods", "d. Endeavor"],
+    a12: ["a. Takuma Ogata", "b. Natsu Maebara", "c. Yagi Toshinori", "d. Akeno Harada"],
+    a13: ["a. True", "b. False"],
+    // a14: ["a. ", "b. ", "c. ", "d. "],
+    // a15: ["a. ", "b. ", "c. ", "d. "]
 };
 
-var questionHeaderText = document.getElementById("#question-header-text");
-var questionText = document.getElementById("#question-text");
-var choicesText = document.getElementById("#choices-text");
-var answerText = document.getElementById("#answer-text");
-
 // Functions
-// 1. Push start button to begin trivia
-// 2. Display question with choices and timer of 20 seconds
-// 3. Read player's choice and determine whether right or wrong
-// 4. Display "correct" message if guessed right/display "wrong" message with correct answer if guessed wrong
-//      a. display "Time's Up!" with correct answer message if timer gets to 0
-// 5. Change to next question after 5 seconds and reset timer
-// 6. At end of questions, display number guessed right and number guessed wrong
-//      a. Provide reset button to take quiz again
+$(document).ready(function() {
+    // 1. Push start button to begin trivia
+    $("#start-button").on("click", function() {
+        // 2. Display question with choices and timer of 20 seconds
+        $("#intro-text").hide();
+        $("#spoiler-warning").hide();
+        $("#start-button").hide();
+        $("#question-header-text").text(questionHeaders[0]);
+        $("#question-text").text(questions.q1);
+        $("#choiceA").text(answers.a1[0])
+        $("#choiceB").text(answers.a1[1])
+        $("#choiceC").text(answers.a1[2])
+        $("#choiceD").text(answers.a1[3])
+    })
+    // 3. Read player's choice and determine whether right or wrong
+    
+    // 4. Display "correct" message if guessed right/display "wrong" message with correct answer if guessed wrong
+    //      a. display "Time's Up!" with correct answer message if timer gets to 0
+    // 5. Change to next question after 5 seconds and reset timer
+    // 6. At end of questions, display number guessed right and number guessed wrong
+    //      a. Provide reset button to take quiz again
+});
