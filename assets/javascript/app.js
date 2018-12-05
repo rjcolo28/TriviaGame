f6398fa1ea5319f02efdd6d9ab3ece1741020f41
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-12-05 11:10:29 -0500

    HTML and JS pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Global Variables
+
+// Functions
+// 1. Push start button to begin trivia
+// 2. Display question with choices and timer of 20 seconds
+// 3. Read player's choice and determine whether right or wrong
+// 4. Display "correct" message if guessed right/display "wrong" message with correct answer if guessed wrong
+//      a. display "Time's Up!" with correct answer message if timer gets to 0
+// 5. Change to next question after 5 seconds and reset timer
+// 6. At end of questions, display number guessed right and number guessed wrong
+//      a. Provide reset button to take quiz again
