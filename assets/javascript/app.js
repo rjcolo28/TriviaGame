0e69deb644d391ec2cdf2947c515ded547a40bf1
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-12-08 00:25:19 -0500

    JS logic not so fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,48 +28,75 @@ var questions = [
         choices: ["a. All Might", "b. Eraserhead", "c. Best Jeanist", "d. Mt. Lady"],
         answer: "a. All Might"
     },
+    {
+        question: "What is the name of the quirk Izuku Midoriya inherits from All Might?",
+        choices: ["a. All for One ", "b. One for All", "c. Super Smash", "d. Justice Power"],
+        answer: "b. One for All"
+    },
+    {
+        question: "True or False? Katsuki Bakugo’s quirk, Explosion, is sweating a nitroglycerin-like substance from his palms and igniting it to create...well, explosions.",
+        choices:["True", "False", "", ""],
+        answer: "True"
+    },
+    {
+        question: "How does Iida Tenya have super speed?",
+        choices: ["a. A pseudo-scientfic mix of chemicals and a lightning strike.", "b. He has engines on his calves.", "c. He's just really fast"],
+        answer: "b. He has engines in his calves."
+    },
+    {
+        question: "Who is the smartest (grade-wise) student in Class 1-A?",
+        choices: ["a. Izuku Midoriya", "b. Shoto Todoroki", "c. Momo Yaoyorozu", "d. Tenya Iida"],
+        answer: "c. Momo Yaoyorozu"
+    },
+    {
+        question: "What is Shota Aizawa’s quirk?",
+        choices: ["a. Erasure", "b. Delete", "c. Nullify", "d. Sleep"],
+        answer: "a. Erasure"
+    },
+    {
+        question: "Who is the leader of the League of Villains?",
+        choices: ["a. Dabi", "b. Kurogiri", "c. All for One", "d. Tomura Shigaraki"],
+        answer: "d. Tomura Shigaraki"
+    },
+    {
+        question: "True or False? Shoto Todoroki resents his father for neglecting him as a child.",
+        choices: ["True", "False", "", ""],
+        answer: "False. Shoto resents his father for the physical abuse he and his mother were put through."
+    },
+    {
+        question: "Who is the first superhero we see in action in My Hero Academia?",
+        choices: ["a. Kamui Woods", "b. Mt. Lady", "c. Death Arms", "d. Backdraft"],
+        answer: "c. Death Arms"
+    },
+    {
+        question: "Who becomes the new Number 1 hero after All Might’s retirement?",
+        choices: ["a. Best Jeanist", "b. Gang Orca", "c. Kamui Woods", "d. Endeavor"],
+        answer: "d. Endeavor"
+    },
+    {
+        question: "What is All Might’s real name?",
+        choices: ["a. Takuma Ogata", "b. Natsu Maebara", "c. Yagi Toshinori", "d. Akeno Harada"],
+        answer: "c. Yagi Toshinori"
+    },
+    {
+        question: "True or False? One for All has been passed down through 8 generations.",
+        choices: ["True", "False", "", ""],
+        answer: "False. One for All has gone through 9 generations."
+    },
     // {
-    //     question: "What is the name of the quirk Izuku Midoriya inherits from All Might?",
-    //     choices: ["a. All for One ", "b. One for All", "c. Super Smash", "d. Justice Power"],
-    //     answer: "b. One for All"
-    // }
+    //     question: ,
+    //     choices: ,
+    //     answer:
+    // },
+    // {
+    //     question: ,
+    //     choices: ,
+    //     answer:
+    // },
 ];
 
-    // q2: "Who is the Number 1 Hero?",
-    // q3: "What is the name of the quirk Izuku Midoriya inherits from All Might?",
-    // q4: "True or False? Katsuki Bakugo’s quirk, Explosion, is sweating a nitroglycerin-like substance from his palms and igniting it to create...well, explosions.",
-    // q5: "How does Iida Tenya have super speed?",
-    // q6: "Who is the smartest (grade-wise) student in Class 1-A?",
-    // q7: "What is Shota Aizawa’s quirk?",
-    // q8: "Who is the leader of the League of Villains?",
-    // q9: "True or False? Shoto Todoroki resents his father for neglecting him as a child.",
-    // q10: "Who is the first superhero we see in action in My Hero Academia?",
-    // q11: "Who becomes the new Number 1 hero after All Might’s retirement?",
-    // q12: "What is All Might’s real name?",
-    // q13: "True or False? One for All has been passed down through 8 generations.",
-    // q14: "",
-    // q15: "",
-
-
-// var answers = {
-//     a1: ["a. superpowers", "b. powers", "c. quirks", "d. magic"],
-//     a2: ["a. All Might", "b. Eraserhead", "c. Best Jeanist", "d. Mt. Lady"],
-//     a3: ["a. All for One ", "b. One for All", "c. Super Smash", "d. Justice Power"],
-//     a4: ["a. True", "b. False"],
-//     a5: ["a. A pseudo-scientfic mix of chemicals and a lightning strike.", "b. He has engines on his calves.", "c. He's just really fast"],
-//     a6: ["a. Izuku Midoriya", "b. Shoto Todoroki", "c. Momo Yaoyorozu", "d. Tenya Iida"],
-//     a7: ["a. Erasure", "b. Delete", "c. Nullify", "d. Sleep"],
-//     a8: ["a. Dabi", "b. Kurogiri", "c. All for One", "d. Tomura Shigaraki"],
-//     a9: ["a. True", "b. False"],
-//     a10: ["a. Kamui Woods", "b. Mt. Lady", "c. Death Arms", "d. Backdraft"],
-//     a11: ["a. Best Jeanist", "b. Gang Orca", "c. Kamui Woods", "d. Endeavor"],
-//     a12: ["a. Takuma Ogata", "b. Natsu Maebara", "c. Yagi Toshinori", "d. Akeno Harada"],
-//     a13: ["a. True", "b. False"],
-    // a14: ["a. ", "b. ", "c. ", "d. "],
-    // a15: ["a. ", "b. ", "c. ", "d. "]
-// };
-var number = 0
-var timer = 20
+var number = 0;
+var timer = 20;
 // Functions
 $(document).ready(function() {
     // 1. Push start button to begin trivia
@@ -78,12 +105,6 @@ $(document).ready(function() {
         $(this).hide();
         $("#intro-text").hide();
         $("#spoiler-warning").hide();
-        var timer = setInterval(function() {}, 1000 * 20);
-        var userGuess;
-        $(".choice").on("click", function() {
-            userGuess = $(this).text();
-            console.log(userGuess);
-        })
         function display(i) {
             $("#question-header-text").text(questionHeaders[i]);
             $("#question-text").text(questions[i].question);
@@ -91,15 +112,21 @@ $(document).ready(function() {
             $("#choiceB").text(questions[i].choices[1]);
             $("#choiceC").text(questions[i].choices[2]);
             $("#choiceD").text(questions[i].choices[3]);
-            if (userGuess === questions[i].answer) {
-                number++;
-                setTimeout(function() {display(number)}, 1000 * 5);
-            }
-            else if (userGuess != questions[i].answer) {
-                number++;
-                display(number);
-
-            }
+            var userGuess;
+            $(".choice").on("click", function click() {
+                userGuess = $(this).text();
+                if (userGuess === questions[i].answer) {
+                    number++;
+                    display(number);
+                    console.log("correct");
+                    // setTimeout(function() {display(number)}, 1000 * 5);
+                }
+                else if (userGuess != questions[i].answer) {
+                    number++;
+                    console.log("incorrect")
+                    // setTimeout(function() {display(number)}, 1000 * 5);
+                }
+            })
         
         };
         display(number);
